647c5a9204147b960602db617053e7e2bfef9c86
marquinho94 <marcoalanmiranda@gmail.com>
2023-04-17 13:57:02 -0300

    blink de inicio

--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,10 @@ void setup() {
 }
 
 void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+
   // put your main code here, to run repeatedly:
 }
