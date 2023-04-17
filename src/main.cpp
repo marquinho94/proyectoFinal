adb953ba725efa4272ab590fe7975b9890b89e2d
marquinho94 <marcoalanmiranda@gmail.com>
2023-04-17 16:23:10 -0300

    comin final

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,22 @@
 #include <Arduino.h>
 
 void setup() {
+  // pinMode(1,OUTPUT);
+  pinMode(13,OUTPUT);
+  Serial.begin(9600);
+
   // put your setup code here, to run once:
   // Hola, estoy escribiendo mi codigo base 
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+  // digitalWrite(13, HIGH);
+  Serial.println("Hola, el led deberia encender");
+  
+  delay(500);
+  // digitalWrite(13, LOW);
+  Serial.println("led apagado");
+  delay(500);
 
   // put your main code here, to run repeatedly:
 }
